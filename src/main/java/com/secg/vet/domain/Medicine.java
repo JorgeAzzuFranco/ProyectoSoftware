5bc0bfffc531101ab35f70d6da3dcdc0b8bab1ed
JorgeAzzuFranco <00074216@uca.edu.sv>
2019-10-13 18:55:29 -0600

    Bootstrap Library Added and minor fixes

--- src/main/java/com/secg/vet/domain/Medicine.java
+++ src/main/java/com/secg/vet/domain/Medicine.java
@@ -12,21 +12,67 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="medicina")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Medicine {
 
     @Id
-    Integer id;
+    private Integer id;
     @Column(name="nombre")
-    String name;
+    private String name;
     @Column(name="dosis")
-    String dose;
+    private String dose;
     @Column(name="cantidad")
-    Integer quantity;
+    private Integer quantity;
     @Column(name="descripcion")
-    String description;
+    private String description;
 
+    public Medicine() {
+    }
+
+    public Medicine(Integer id, String name, String dose, Integer quantity, String description) {
+        this.id = id;
+        this.name = name;
+        this.dose = dose;
+        this.quantity = quantity;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDose() {
+        return dose;
+    }
+
+    public void setDose(String dose) {
+        this.dose = dose;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
