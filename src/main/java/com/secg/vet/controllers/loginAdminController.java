05338d10305f59757579e7346c4b13a855216e17
ilianairaheta <00013014@uca.edu.sv>
2019-10-14 00:04:14 -0600

    en desarrollo
    Login en construccion

---
+++ src/main/java/com/secg/vet/controllers/loginAdminController.java
@@ -0,0 +1,17 @@
+package com.secg.vet.controllers;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
