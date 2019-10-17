9287f2a04d7e13df7a5bff9437c43150ab753405
salvadorc94 <secg.1994@gmail.com>
2019-10-17 17:36:53 -0600

    Añadido los dominios para el almacen y proveedor.

---
+++ src/main/java/com/secg/vet/domain/Almacen.java
@@ -0,0 +1,74 @@
+package com.secg.vet.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(schema="public",name="almacen")
+public class Almacen {
+
+    @Id
+    @GeneratedValue(generator = "almacen_id_seq", strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "almacen_id_seq", sequenceName = "public.almacen_id_seq", allocationSize = 1)
+    @Column(name="id")
+    private int pk_almacen;
+
+    @NotEmpty(message="Ingrese el código")
+    @NotNull
+    @Column(name="sku")
+    private String sku;
+
+    @NotEmpty(message="Ingrese el nombre del producto")
+    @NotNull
+    @Column(name="nombre_producto")
+    private String nombre_producto;
+
+    @NotNull
+    @Column(name="cantidad")
+    private int cantidad;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_proveedor")
+    private Proveedor proveedor;
+
+    public int getPk_almacen() {
+        return pk_almacen;
+    }
+
+    public void setPk_almacen(int pk_almacen) {
+        this.pk_almacen = pk_almacen;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getNombre_producto() {
+        return nombre_producto;
+    }
+
+    public void setNombre_producto(String nombre_producto) {
+        this.nombre_producto = nombre_producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+}
---
+++ src/main/java/com/secg/vet/domain/Proveedor.java
@@ -0,0 +1,55 @@
+package com.secg.vet.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(schema="public",name="proveedor")
+public class Proveedor {
+
+    @Id
+    @GeneratedValue(generator = "proveedor_id_seq", strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "proveedor_id_seq", sequenceName = "public.proveedor_id_seq", allocationSize = 1)
+    @Column(name="id")
+    private int pk_proveedor;
+
+    @Column(name="nombre_proveedor")
+    private String nombre_proveedor;
+
+    @Column(name="descripcion")
+    private String descripcion;
+
+    @Column(name="telefono_principal")
+    private String telefono_principal;
+
+    public int getPk_proveedor() {
+        return pk_proveedor;
+    }
+
+    public void setPk_proveedor(int pk_proveedor) {
+        this.pk_proveedor = pk_proveedor;
+    }
+
+    public String getNombre_proveedor() {
+        return nombre_proveedor;
+    }
+
+    public void setNombre_proveedor(String nombre_proveedor) {
+        this.nombre_proveedor = nombre_proveedor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTelefono_principal() {
+        return telefono_principal;
+    }
+
+    public void setTelefono_principal(String telefono_principal) {
+        this.telefono_principal = telefono_principal;
+    }
+}
