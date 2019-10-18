f23bbfc78d59906c1e6c68493502186f5a921e4e
jorgeazzufranco <2FEB4E81j>
2019-10-18 15:30:05 -0600

    Removed unused imports

--- src/main/java/com/secg/vet/domain/Medicine.java
+++ src/main/java/com/secg/vet/domain/Medicine.java
@@ -1,10 +1,5 @@
 package com.secg.vet.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
