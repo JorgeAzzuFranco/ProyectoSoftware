1c8103141951a4f3dbdc36541efe145a0f1f8aae
salvadorc94 <secg.1994@gmail.com>
2019-11-28 23:30:35 -0600

    tarea8, editar y eliminar Proveedores...

--- src/main/java/com/secg/vet/controllers/ProviderController.java
+++ src/main/java/com/secg/vet/controllers/ProviderController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,6 +22,22 @@ public class ProviderController {
         List<Provider> providers = providerService.findAll();
         model.addAttribute("list",providers);
         return "/admin/listProvider";
+    }
+
+    @GetMapping("/admin/deleteProvider")
+    public String deleteProvider(Model model,@RequestParam(value="id",required = true) String id){
+        Provider provider = providerService.findOne(Integer.parseInt(id));
+        providerService.delete(provider);
+        List<Provider> providers = providerService.findAll();
+        model.addAttribute("list",providers);
+        return "/admin/listProvider";
+    }
+
+    @GetMapping("/admin/editProvider")
+    public String editProvider(Model model,@RequestParam(value="id",required = true) String id){
+        Provider provider = providerService.findOne(Integer.parseInt(id));
+        model.addAttribute("provider",provider);
+        return "/admin/addProvider";
     }
 
     @GetMapping("/admin/addProvider")
